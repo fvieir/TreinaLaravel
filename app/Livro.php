8307b24e33a027f427833c637e30f5eeb55b6a0c
Fabricio Dourado <65264206+fvieir@users.noreply.github.com>
2021-07-10 18:21:55 -0300

    Alteração do Model Livro

--- app/Livro.php
+++ app/Livro.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Livro extends Model
 {
-    //
+    protected $fillable = [
+        'titulo','descricao'
+    ];
+
+    public function pesqLivro()
+    {
+        return $this->all();
+    }
 }
