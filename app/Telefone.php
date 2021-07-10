60437981365a575c35830f7c0761f6fe3749e6e0
Fabricio Dourado <65264206+fvieir@users.noreply.github.com>
2021-07-10 18:20:38 -0300

    Adição Model Telefone

---
+++ app/Telefone.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Telefone extends Model
+{
+    protected $fillable = [ 
+        'cliente_id',
+        'descricao',
+        'telefone'
+    ];
+
+    public function cliente()
+    {
+        return $this->belongsTo('App\Cliente');
+    }
+}
